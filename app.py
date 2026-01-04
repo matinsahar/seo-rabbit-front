e8c30a398381bad787c099f18d67e0afd619eeab
matinsahar <49645234+matinsahar@users.noreply.github.com>
2026-01-04 15:50:42 +0330

    typo

--- app.py
+++ app.py
@@ -415,4 +415,4 @@ def submit_campaign(n_clicks, posts_needed, start_date, end_date, site_data):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
